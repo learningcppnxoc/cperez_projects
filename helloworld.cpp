8be607feebff9658164be2ef48b70dd4474040e8
Carlos Perez <solracias@gmail.com>
2019-06-11 13:16:15 -0700

    new hello world
    done

---
+++ helloworld.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    cout << "Hello, World!" << endl;
+	
+	
+	int firstValue;
+	int secondValue;
+	
+	firstValue = 1;
+	secondValue = 2;
+	
+	cout << "Number n1: " << firstValue << endl;
+	cout << "Number n2: " << secondValue << endl;
+	int sumOfN;
+	
+	sumOfN = firstValue + secondValue;
+	
+	cout << "sum is: " << firstValue << " plus " << secondValue << " result is " << sumOfN << endl;
+	
+	
+    return 0;
+}
